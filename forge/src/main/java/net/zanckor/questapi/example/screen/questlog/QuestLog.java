86e31ca657ced1b51cdc5c2d77c5866805949e4a
Zanckor <zanckor002@gmail.com>
2023-08-06 13:51:38 +0200

    Pushed QuestLog changes

--- forge/src/main/java/net/zanckor/questapi/example/screen/questlog/QuestLog.java
+++ forge/src/main/java/net/zanckor/questapi/example/screen/questlog/QuestLog.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
@@ -50,6 +49,7 @@ public class QuestLog extends AbstractQuestLog {
     float sin;
 
     UserQuest selectedQuest;
+
 
     public QuestLog(Component component) {
         super(component);
@@ -140,12 +140,13 @@ public class QuestLog extends AbstractQuestLog {
         addWidget(prevPage);
         addWidget(nextPage);
 
-        textButton.setValue("Quest name");
+        textButton.setValue("Quest name - Press Enter");
         SendQuestPacket.TO_SERVER(new RequestActiveQuests());
     }
 
     @Override
     public boolean keyReleased(int x, int y, int p_94717_) {
+        //Update the search result of quests by name each time player press enter
         if (textButton != null && x == 257 && y == 28) {
             questSearch = textButton.getValue();
             init();
@@ -156,21 +157,30 @@ public class QuestLog extends AbstractQuestLog {
 
     @Override
     public boolean mouseClicked(double x, double y, int p_94697_) {
+        //Once you click your mouse, the search bar will restart
         textButton.setValue("");
 
         return super.mouseClicked(x, y, p_94697_);
     }
 
+    /**
+     * Renders quest log textures and text as goals, quest titles...
+     */
     @Override
     public void render(@NotNull GuiGraphics graphics, int x, int y, float partialTicks) {
         float scale = ((float) width) / 500;
+
+        //Changes the scale of gui so the enableScissor won't bug on different sizes. Then come back to previousScale
         PoseStack poseStack = graphics.pose();
 
+        //Render background texture
         Minecraft.getInstance().getProfiler().push("background");
         graphics.blit(QUEST_LOG, (int) (xScreenPos - (imageWidth / 2)), (int) yScreenPos, 0, 0, imageWidth, imageHeight, imageWidth, imageHeight);
 
+        //Render quest data as current progress, goals...
         renderQuestData(graphics, poseStack);
 
+        //Render the text for "Quest Title" and "Quest Info"
         MCUtilClient.renderText(graphics, poseStack, (float) (xScreenPos - imageWidth / 2.9), (float) (yScreenPos + (20 * scale)), 0, scale, 40, "Quest Title", font);
         MCUtilClient.renderText(graphics, poseStack, (float) (xScreenPos + imageWidth * 0.135), (float) (yScreenPos + (20 * scale)), 0, scale, 40, "Quest Info", font);
 
@@ -179,11 +189,15 @@ public class QuestLog extends AbstractQuestLog {
         super.render(graphics, x, y, partialTicks);
     }
 
-    public void renderQuestData(GuiGraphics graphics, PoseStack poseStack) {
-        if (selectedQuest == null || selectedQuest.isCompleted())
-            return;
 
-        HashMap<String, List<UserGoal>> userQuestHashMap = new HashMap<>();
+    /**
+     * Renders quest data as quest goals and the progress
+     */
+    public void renderQuestData(GuiGraphics graphics, PoseStack poseStack) {
+        //If no quest is selected, or it is completed, do not keep rendering the data
+        if (selectedQuest == null || selectedQuest.isCompleted()) return;
+
+        HashMap<String, List<UserGoal>> userQuestHashMap = createGoalTypesMap();
         int xPosition = (int) (width / 1.925);
         float scale = ((float) width) / 700;
 
@@ -191,57 +205,73 @@ public class QuestLog extends AbstractQuestLog {
         poseStack.translate(xPosition, yScreenPos + ((((float) width) / 500) * 40), 0);
         poseStack.scale(scale, scale, 0);
 
-        //Gets all quest types on json and creates a HashMap with a list of goals
-        for (UserGoal questGoal : selectedQuest.getQuestGoals()) {
-            String type = questGoal.getType();
-            List<UserGoal> questGoalList = userQuestHashMap.get(type);
-
-
-            if (questGoalList == null) {
-                questGoalList = new ArrayList<>();
-            }
-
-            questGoalList.add(questGoal);
-
-            userQuestHashMap.put(type, questGoalList);
-        }
-
         //Displays quest goals
-        renderTitle(graphics, poseStack, minecraft);
-        renderQuestType(graphics, poseStack, minecraft, userQuestHashMap);
+        renderSelectedQuestTitle(graphics, poseStack, minecraft);
+        renderSelectedQuestData(graphics, poseStack, minecraft, userQuestHashMap);
 
+        //If any goal has a time limit, add it as data, and update it each second to check how much time is left
         if (selectedQuest.hasTimeLimit()) {
             MCUtilClient.renderLine(graphics, poseStack, 0, 0, 30, I18n.get("tracker.questapi.time_limit") + selectedQuest.getTimeLimitInSeconds(), font);
         }
 
-
         poseStack.popPose();
     }
 
-    public void renderTitle(GuiGraphics graphics, PoseStack poseStack, Minecraft minecraft) {
+    HashMap<String, List<UserGoal>> createGoalTypesMap() {
+        HashMap<String, List<UserGoal>> goalTypesMap = new HashMap<>();
+
+      /*Create a key for each QuestType (Kill, Collect, MoveTo...) based on player's quests.
+      *Then for each key add the goals of that type, for example, for Kill key, adds all the goals that consist on
+      kill an entity*/
+        for (UserGoal questGoal : selectedQuest.getQuestGoals()) {
+            String type = questGoal.getType();
+            List<UserGoal> questGoalList = goalTypesMap.get(type);
+
+            //Create a new list for this key if it doesn't already exist
+            if (questGoalList == null) {
+                questGoalList = new ArrayList<>();
+            }
+
+            //Adds the goal to the current list and then update the list to the hashMap
+            questGoalList.add(questGoal);
+            goalTypesMap.put(type, questGoalList);
+        }
+
+        return goalTypesMap;
+    }
+
+
+    /**
+     * Renders the title of selected quest
+     */
+    public void renderSelectedQuestTitle(GuiGraphics graphics, PoseStack poseStack, Minecraft minecraft) {
         String title = I18n.get(selectedQuest.getTitle());
         MCUtilClient.renderLines(graphics, poseStack, 25, 10, 30, I18n.get("tracker.questapi.quest") + title, minecraft.font);
     }
 
-    public void renderQuestType(GuiGraphics graphics, PoseStack poseStack, Minecraft minecraft, HashMap<String, List<UserGoal>> userQuestHashMap) {
+    /**
+     * Renders the data of all goals of selected quest
+     */
+    public void renderSelectedQuestData(GuiGraphics graphics, PoseStack poseStack, Minecraft minecraft, HashMap<String, List<UserGoal>> goalTypesMap) {
         int scissorBottom = (int) (height - (yScreenPos + imageHeight) + 15) * 2;
         int scissorTop = (int) (imageHeight * 1.25) - 15;
 
+        //Enable scissoring for cut rendering if text is out of the box, so it won't render text out of the book
         RenderSystem.enableScissor(width, scissorBottom, (width / 2) + (imageWidth / 2), scissorTop);
 
-        Font font = minecraft.font;
         Player player = minecraft.player;
         poseStack.translate(0, questInfoScroll + 20, 0);
         sin += 0.5;
 
-        for (Map.Entry<String, List<UserGoal>> entry : userQuestHashMap.entrySet()) {
+        //Render the data of each goal, in order of goal type. So all Kill goals will render in a list, in order.
+        for (Map.Entry<String, List<UserGoal>> entry : goalTypesMap.entrySet()) {
             List<UserGoal> questGoalList = entry.getValue();
 
-            //Render quest type
+            //Display text of the goal quest type.
             MCUtilClient.renderLine(graphics, poseStack, 30, 0, 0, 10, Component.literal(I18n.get("tracker.questapi.quest_type") +
                     I18n.get("quest_type." + questGoalList.get(0).getTranslatableType().toLowerCase())).withStyle(ChatFormatting.BLACK), font);
 
-            //Render each quest goal of a single type and render target
+            //Display the data of each goal
             for (UserGoal questGoal : questGoalList) {
                 Enum goalEnum = EnumRegistry.getEnum("TARGET_TYPE_" + questGoal.getType(), EnumRegistry.getTargetType());
 
@@ -252,6 +282,7 @@ public class QuestLog extends AbstractQuestLog {
                 MCUtilClient.renderLine(graphics, poseStack, 30, 0, 0, 10, goalComponentTarget.withStyle(ChatFormatting.ITALIC), font);
             }
 
+            //Translate 10px for display the next goal
             poseStack.translate(0, 10, 0);
         }
 
@@ -259,9 +290,13 @@ public class QuestLog extends AbstractQuestLog {
     }
 
 
+    /**
+     * On scroll mouse wheel, the displayed data of the quest will move up and down, so player will be able to see all text
+     * even if its out of the book
+     */
     @Override
     public boolean mouseScrolled(double mouseX, double mouseY, double scroll) {
-        boolean mouseXInBox = (mouseX > xScreenPos) && (mouseX < (xScreenPos + imageWidth / 2));
+        boolean mouseXInBox = (mouseX > xScreenPos) && (mouseX < (xScreenPos + (double) imageWidth / 2));
         boolean mouseYInBox = (mouseY > yScreenPos) && (mouseY < (yScreenPos + imageHeight));
 
         if (mouseXInBox && mouseYInBox) {
